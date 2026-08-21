052e83c1c8a66fdb6df945662c160d5823200ce2
5jc44jhfj6-jpg <5jc44jhfj6@privaterelay.appleid.com>
2026-08-21 11:13:15 +0300

    Update WebView.swift

--- Huzur Vakti/WebView.swift
+++ Huzur Vakti/WebView.swift
@@ -14,6 +14,8 @@ func createWebView(container: UIView, WKSMH: WKScriptMessageHandler, WKND: WKNav
     userContentController.add(WKSMH, name: "push-permission-request")
     userContentController.add(WKSMH, name: "push-permission-state")
     userContentController.add(WKSMH, name: "push-token")
+    userContentController.add(WKSMH, name: "schedule-local-notification")
+    userContentController.add(WKSMH, name: "clear-local-notifications")
 
     config.userContentController = userContentController
 
